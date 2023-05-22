f64dad1fcec74a0f7f536fe6ad610a2a5d2b3967
patrick-techstarter <134279530+patrick-techstarter@users.noreply.github.com>
2023-05-22 12:46:41 +0200

    Create HA_python.py

---
+++ Python/HA_python.py
@@ -0,0 +1,6 @@
+# To take input from the user
+num = int(input("Darstellung der Multiplikationstabelle für den Wert?  "))
+
+# Iterate 10 times from i = 1 to 10
+for i in range(1, 11):
+   print(num, 'x', i, '=', num*i)
